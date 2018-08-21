75a841f195a45ecdedee9b3108515564fdb295b3
agostinhobritojr <agostinhobritojr@gmail.com>
2018-08-20 21:09:21 -0300

    alo

---
+++ _site/tutorial/pdi/solucoes/canny-fig.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char** argv){
+  vector<int> yrange;
+  vector<int> xrange;
+
+  Mat image, frame, frame32f, frameok;
+
+  int MAXITER=5;
+  int depth, radius;
+  int width, height;
+  
+  ofstream fout;
+
+  
+  image= imread(argv[1],CV_LOAD_IMAGE_GRAYSCALE);
+  
+  if(!image.data){
+	cout << "nao abriu" << argv[1] << endl;
+    cout << argv[0] << " imagem.jpg";
+  }
+
+  width=image.size().width;
+  height=image.size().height;
+
+  xrange.resize(height);
+  yrange.resize(width);
+  
+  iota(xrange.begin(), xrange.end(), 0); 
+  iota(yrange.begin(), yrange.end(), 0);
+ 
+  image.copyTo(frameok);
+  
+  fout.open("saida.fig");
+  fout << "#FIG 3.2  Produced by xfig version 3.2.5c" << endl;
+  fout << "Landscape" << endl;
+  fout << "Center" << endl;
+  fout << "Metric" << endl;
+  fout << "A4" << endl;      
+  fout << "100.00" << endl;
+  fout << "Single" << endl;
+  fout << "-2" << endl;
+  fout << "1200 2" << endl;
+  for(int i=0; i<256; i++){
+    fout << std::dec;
+    fout << "0 " << i+32 << " #";
+    fout << std::hex;
+    fout << setw(2) << setfill('0') << i;
+    fout << setw(2) << setfill('0') << i;
+    fout << setw(2) << setfill('0') << i;
+    fout << endl;
+  }
+  fout << std::dec;
+
+  for(int k=MAXITER; k>=0; k--){
+    Canny(frameok, frame32f, (MAXITER-k+1)*10, (MAXITER-k+1)*30);
+    normalize(frame32f, frame32f, 0.0, 1.0, NORM_MINMAX);
+    frame32f.convertTo(frame,CV_8U);
+    depth = k+50;
+    radius = (k+1)*100;
+    random_shuffle(xrange.begin(), xrange.end());
+    for(auto i : xrange){
+      random_shuffle(yrange.begin(), yrange.end());
+      for(auto j : yrange){
+        if(frame.at<uchar>(i,j) > 0 && rand()%MAXITER >= k){
+          fout << "1 3 0 0 0 ";
+          fout << image.at<uchar>(i,j)+32 << " ";
+          fout << depth;
+          fout << " -1 20 0.000 1 0.0000 ";
+          fout << j*100 << " " << i*100 << " ";
+          fout << radius << " " << radius;
+          fout << " " << j*100 << " " << j*100 << " ";
+          fout << j*100+radius << " " << j*100 << endl;
+        }
+      }
+    }
+  }
+  fout.close();
+  return 0;
+}
