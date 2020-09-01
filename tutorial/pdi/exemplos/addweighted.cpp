8e910f437c95601e2d9b535c4cc87426c7293080
Agostinho Brito <agostinhobritojr@gmail.com>
2020-09-01 09:04:05 -0300

    pdi

--- tutorial/pdi/exemplos/addweighted.cpp
+++ tutorial/pdi/exemplos/addweighted.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
+#include <cstdio>
 #include <opencv2/opencv.hpp>
-
-using namespace cv;
-using namespace std;
 
 double alfa;
 int alfa_slider = 0;
@@ -11,47 +9,47 @@ int alfa_slider_max = 100;
 int top_slider = 0;
 int top_slider_max = 100;
 
-Mat image1, image2, blended;
-Mat imageTop; 
+cv::Mat image1, image2, blended;
+cv::Mat imageTop; 
 
 char TrackbarName[50];
 
 void on_trackbar_blend(int, void*){
  alfa = (double) alfa_slider/alfa_slider_max ;
- addWeighted( image1, alfa, imageTop, 1-alfa, 0.0, blended);
- imshow("addweighted", blended);
+ cv::addWeighted(image1, 1-alfa, imageTop, alfa, 0.0, blended);
+ cv::imshow("addweighted", blended);
 }
 
 void on_trackbar_line(int, void*){
   image1.copyTo(imageTop);
   int limit = top_slider*255/100;
   if(limit > 0){
-	Mat tmp = image2(Rect(0, 0, 256, limit));
-	tmp.copyTo(imageTop(Rect(0, 0, 256, limit)));
+    cv::Mat tmp = image2(cv::Rect(0, 0, 256, limit));
+    tmp.copyTo(imageTop(cv::Rect(0, 0, 256, limit)));
   }
   on_trackbar_blend(alfa_slider,0);
 }
 
 int main(int argvc, char** argv){
-  image1 = imread("blend1.jpg");
-  image2 = imread("blend2.jpg");
+  image1 = cv::imread("blend1.jpg");
+  image2 = cv::imread("blend2.jpg");
   image2.copyTo(imageTop);
-  namedWindow("addweighted", 1);
+  cv::namedWindow("addweighted", 1);
   
-  sprintf( TrackbarName, "Alpha x %d", alfa_slider_max );
-  createTrackbar( TrackbarName, "addweighted",
-				  &alfa_slider,
-				  alfa_slider_max,
-				  on_trackbar_blend );
+  std::sprintf( TrackbarName, "Alpha x %d", alfa_slider_max );
+  cv::createTrackbar( TrackbarName, "addweighted",
+                      &alfa_slider,
+                      alfa_slider_max,
+                      on_trackbar_blend );
   on_trackbar_blend(alfa_slider, 0 );
   
-  sprintf( TrackbarName, "Scanline x %d", top_slider_max );
-  createTrackbar( TrackbarName, "addweighted",
-				  &top_slider,
-				  top_slider_max,
-				  on_trackbar_line );
+  std::sprintf( TrackbarName, "Scanline x %d", top_slider_max );
+  cv::createTrackbar( TrackbarName, "addweighted",
+                      &top_slider,
+                      top_slider_max,
+                      on_trackbar_line );
   on_trackbar_line(top_slider, 0 );
 
-  waitKey(0);
+  cv::waitKey(0);
   return 0;
 }
