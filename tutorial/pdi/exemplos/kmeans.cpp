cce636cce833ca2296f01b59c8f58353678aafba
Agostinho Brito <agostinhobritojr@gmail.com>
2022-10-11 10:55:46 -0300

    kmeans

--- tutorial/pdi/exemplos/kmeans.cpp
+++ tutorial/pdi/exemplos/kmeans.cpp
@@ -1,48 +1,41 @@
-#include <opencv2/opencv.hpp>
 #include <cstdlib>
+#include <opencv2/opencv.hpp>
 
-using namespace cv;
+int main(int argc, char** argv) {
+  int nClusters = 8, nRodadas = 5;
 
-int main( int argc, char** argv ){
-  int nClusters = 8;
-  Mat rotulos;
-  int nRodadas = 5;
-  Mat centros;
+  cv::Mat rotulos, centros;
 
-  if(argc!=3){
-	exit(0);
+  if (argc != 2) {
+    exit(0);
   }
-  
-  Mat img = imread( argv[1], cv::IMREAD_COLOR);
-  Mat samples(img.rows * img.cols, 3, CV_32F);
 
-  for( int y = 0; y < img.rows; y++ ){
-    for( int x = 0; x < img.cols; x++ ){
-      for( int z = 0; z < 3; z++){
-        samples.at<float>(y + x*img.rows, z) = img.at<Vec3b>(y,x)[z];
-	  }
-	}
+  cv::Mat img = cv::imread(argv[1], cv::IMREAD_COLOR);
+  cv::Mat samples(img.rows * img.cols, 3, CV_32F);
+
+  for (int y = 0; y < img.rows; y++) {
+    for (int x = 0; x < img.cols; x++) {
+      for (int z = 0; z < 3; z++) {
+        samples.at<float>(y + x * img.rows, z) = img.at<cv::Vec3b>(y, x)[z];
+      }
+    }
   }
-  
-  kmeans(samples,
-		 nClusters,
-		 rotulos,
-		 TermCriteria(cv::TermCriteria::EPS, 10000, 0.0001),
-		 nRodadas,
-		 cv::KMEANS_PP_CENTERS,
-		 centros );
 
+  cv::kmeans(samples, nClusters, rotulos,
+             cv::TermCriteria(cv::TermCriteria::EPS | cv::TermCriteria::COUNT,
+                              10000, 0.0001),
+             nRodadas, cv::KMEANS_PP_CENTERS, centros);
 
-  Mat rotulada( img.size(), img.type() );
-  for( int y = 0; y < img.rows; y++ ){
-    for( int x = 0; x < img.cols; x++ ){ 
-	  int indice = rotulos.at<int>(y + x*img.rows,0);
-	  rotulada.at<Vec3b>(y,x)[0] = (uchar) centros.at<float>(indice, 0);
-	  rotulada.at<Vec3b>(y,x)[1] = (uchar) centros.at<float>(indice, 1);
-	  rotulada.at<Vec3b>(y,x)[2] = (uchar) centros.at<float>(indice, 2);
-	}
+  cv::Mat rotulada(img.size(), img.type());
+  for (int y = 0; y < img.rows; y++) {
+    for (int x = 0; x < img.cols; x++) {
+      int indice = rotulos.at<int>(y + x * img.rows, 0);
+      rotulada.at<cv::Vec3b>(y, x)[0] = (uchar)centros.at<float>(indice, 0);
+      rotulada.at<cv::Vec3b>(y, x)[1] = (uchar)centros.at<float>(indice, 1);
+      rotulada.at<cv::Vec3b>(y, x)[2] = (uchar)centros.at<float>(indice, 2);
+    }
   }
-  imshow( "clustered image", rotulada );
-  imwrite(argv[2], rotulada);
-  waitKey( 0 );
+  cv::imshow("kmeans", rotulada);
+  cv::imwrite(argv[2], rotulada);
+  cv::waitKey();
 }
